a6403dcbbf097b2262555bad92e5e1df2e7edf19
Arsen Martirosyan <arsenmartirosyan001@gmail.com>
2019-04-26 13:18:05 +0400

    Search functionality

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,6 +14,7 @@
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
 </head>
 <body>
 <div id="app">
--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,7 @@ Route::prefix('auth')->group(function () {
         Route::post('logout', 'AuthController@logout');
     });
 });
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::get('users', 'UserController@index')->middleware('isAdmin');
+    Route::get('users/{id}', 'UserController@show')->middleware('isAdminOrSelf');
+});
